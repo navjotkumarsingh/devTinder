3fe5d4680f9cbdd721bbc6fe541bfe637990e530
Navjot Kumar singh <navjotkumarsingh81@gmail.com>
2025-07-01 11:25:07 +0530

    Exploring GET,POST,Delete

--- src/app.js
+++ src/app.js
@@ -5,16 +5,38 @@ const express = require('express');
 const app = express();
 
 // Request Handler
-app.use("/test",(req,res)=>{
+// app.use("/",(req,res)=>{     // "/" this will handle all the handler that start with / at first index.
+//     res.send("Hello from the main section");
+// })
+// app.use((req,res)=>{
+//     res.send("Hello from the main section");
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello from the hello section");
+// })
+
+
+// This will only match GET call to "/user".
+app.get("/user",(req,res) => {
+    res.send({firstname: "Navjot", lastname: "Singh"})
+})
+
+app.post("/user",(req,res)=>{
+    //Saving data to db
+    res.send("Data is sucessefully saved to database");
+})
+
+
+app.delete("/user",(req,res)=>{
+    res.send("Data is deleted sucessfully");
+})
+
+
+// This will match all the HTTP method API call to the "/test"
+app.use("test",(req,res)=>{
     res.send("Hello from the test section");
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the hello section");
-})
-app.use((req,res)=>{
-    res.send("Hello from the server");
-})
 
 app.listen(3000, ()=>{
     console.log("Server is sucessfully listenining on port 3000");
