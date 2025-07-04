a421abf64e354627dee88783aa378eb9ae806407
Navjot Kumar singh <navjotkumarsingh81@gmail.com>
2025-07-04 13:03:40 +0530

    Connect database

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@
 const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async (req,res)=>{
+    const user = new User({
+        firstName: "virat",
+        lastName: "singh",
+        emailId: "navjot@gmail.com",
+        password: "navjot@123"
+    })
 
-connectDB().then(() => {
+    try{
+    await user.save(); // This function return a promise.
+    res.send("User added Sucessfully");
+    }catch (err){
+        res.status(400).send("Error Saving the user..."+err.message)
+    }
+    // Creating the new intance of the user model
+    // const user = new User(userObj);
+});
+
+connectDB()
+.then(() => {
     console.log("Database connection establish");
     app.listen(7777, ()=>{
     console.log("Server is sucessfully listenining on port 7777");
