bc9ef2580fbab0eb228b65c960aec4f0b9c9f310
Navjot Kumar singh <navjotkumarsingh81@gmail.com>
2025-07-04 12:16:07 +0530

    add Database.js

--- src/app.js
+++ src/app.js
@@ -1,80 +1,17 @@
 //Use nodemon for automatic refresh server --use:-- "nodemon app.js "
-
 const express = require('express');
-
+const connectDB = require("./config/database");
 const app = express();
 
-// Request Handler
-// app.use("/",(req,res)=>{     // "/" this will handle all the handler that start with / at first index.
-//     res.send("Hello from the main section");
-// })
-// app.use((req,res)=>{
-//     res.send("Hello from the main section");
-// })
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello from the hello section");
-// })
 
-
-// This will only match GET call to "/user".
-// app.get("/user",(req,res) => {
-//     res.send({firstname: "Navjot", lastname: "Singh"})
-// })
-
-// app.post("/user",(req,res)=>{
-//     //Saving data to db
-//     res.send("Data is sucessefully saved to database");
-// })
-
-
-// app.delete("/user",(req,res)=>{
-//     res.send("Data is deleted sucessfully");
-// })
-
-
-// // This will match all the HTTP method API call to the "/test"
-// app.use("test",(req,res)=>{
-//     res.send("Hello from the test section");
-// })
-
-//Regular routing expression
-
-// It will work for "abc" and "ac" both 
-
-// app.get("/ab?c",(req,res)=>{
-//     res.send("Hello worls=d!");
-// })
-
-
-
-// app.get("/ab+c",(req,res)=>{
-//     res.send("Hello worls=d!");
-// })
-
-
-// app.get("/ab*cd",(req,res)=>{
-//     res.send("Hello worls=d!");
-// })
-
-
-//Regix:-- any of the part contain this regix this will route.
-// app.get(/a/,(req,res)=>{
-//     res.send("Hello world!");
-// })
-
-// app.get(/.*fly$/,(req,res)=>{
-//     res.send("Hello world!");
-// })
-
-
-app.get("/user/:userid/:name/:password",(req,res)=>{
-    // console.log(req.query);
-    console.log(req.params);
-    
-    res.send({firstname: "Navjot", lastname: "Singh"});
+connectDB().then(() => {
+    console.log("Database connection establish");
+    app.listen(7777, ()=>{
+    console.log("Server is sucessfully listenining on port 7777");
+}); 
+})
+.catch(err=>{
+    console.log("Database can't be connected");
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is sucessfully listenining on port 3000");
-});
 
